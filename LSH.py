427337e1e61971ed9210e48299e31d187a694962
putgun <112839886+putgun@users.noreply.github.com>
2025-05-09 15:51:26 +0800

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -5,7 +5,7 @@ st.markdown("""
     <style>
     #MainMenu {visibility: hidden;}
     [data-testid="stToolbar"] {visibility: hidden !important;}
-    footer {visibility: hidden !important;}
+    footer {display: none !important;}
     header {visibility: hidden;}
     .viewerBadge_container__1QSob,
     .viewerBadge_link__1S137,
