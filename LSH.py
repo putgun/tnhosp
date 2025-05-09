9708649bfed795941775aede07997b1a76ab5990
putgun <112839886+putgun@users.noreply.github.com>
2025-05-09 16:17:44 +0800

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -13,7 +13,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# st.title("Display Excel Data with Streamlit")
+# st.title("台南醫院報帳請款")
 #
 # upload_file = st.file_uploader("Choose an Excel file", type=['xlsx'])
 # if upload_file is not None:
