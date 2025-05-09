5c1b896c9ed23062d8708142a5d26b448e33f84a
putgun <112839886+putgun@users.noreply.github.com>
2025-05-09 15:55:33 +0800

    Update LSH.py

--- LSH.py
+++ LSH.py
@@ -9,7 +9,7 @@ st.markdown("""
     header {visibility: hidden;}
     .viewerBadge_container__1QSob,
     .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {display: none !important;
+    .viewerBadge_text__1JaDK {display: none !important;}
     </style>
 """, unsafe_allow_html=True)
 
